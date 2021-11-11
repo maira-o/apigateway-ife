49dc9bec9a12ead267ef3e97ee039308ed05908b
Maira de Oliveira da Silva <maira.silva@dbserver.com.br>
2021-11-11 20:20:32 -0300

    update folder

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(logger('dev'));
  
 function selectProxyHost(req) {
     if (req.path.startsWith('/usuario'))
-        return 'http://localhost:3000/';
+        return 'http://localhost:5100/';
     else if (req.path.startsWith('/educador'))
-        return 'http://localhost:3001/';
+        return 'http://localhost:5200/';
     else if (req.path.startsWith('/crianca'))
         return 'http://localhost:5000/';
 }
