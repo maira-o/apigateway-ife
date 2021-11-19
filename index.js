276b86955da0bf9c1d23cd2bb6c7b75de5ef63c8
Maira de Oliveira da Silva <maira.silva@dbserver.com.br>
2021-11-19 10:47:03 -0300

    adicionando variaveis de ambiente

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 //index.js
 require("dotenv-safe").config();
 const jwt = require('jsonwebtoken');
+const dotenv    = require('dotenv');
 const httpProxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 var logger = require('morgan');
  
 app.use(logger('dev'));
+
+dotenv.config();
  
 function selectProxyHost(req) {
     if (req.path.startsWith('/usuario'))
-        return 'http://localhost:5100/';
+        return process.env.APP_USUARIO_URL;
     else if (req.path.startsWith('/educador'))
-        return 'http://localhost:5200/';
+        return process.env.APP_EDUCADOR_URL;
     else if (req.path.startsWith('/crianca'))
-        return 'http://localhost:5000/';
+        return process.env.APP_CRIANCA_URL;
 }
  
 app.use((req, res, next) => {
     httpProxy(selectProxyHost(req))(req, res, next);
 });
+
+const port = process.env.PORT;
  
-app.listen(10000, () => {
+app.listen(port, () => {
     console.log('IFE API Gateway is running! :)');
 });
